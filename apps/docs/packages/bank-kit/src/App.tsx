e1b858094af7356cbddaa60caea6e35f886de3ce
satish-solera <satishsolera54@gmail.com>
2026-07-14 15:22:21 +0530

    add: project initilize with vite react

---
+++ apps/docs/packages/bank-kit/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+  return (
+   <div className="">
+    <h1 className='text-7xl text-red-300'>
+      hi
+    </h1>
+
+   </div>
+  )
+}
+
+export default App
