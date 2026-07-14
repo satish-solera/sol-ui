b7cb26c78f41f53e1d3347a256c9da0fec51840e
satish-solera <satishsolera54@gmail.com>
2026-07-14 12:29:56 +0530

    fix: hero section

--- apps/docs/src/components/ui/button.tsx
+++ apps/docs/src/components/ui/button.tsx
@@ -3,18 +3,24 @@ import { cn } from "@/lib/utils/cn";
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   className?: string;
+  typeOfBtn?: string;
+ 
 };
 
 export const Button = ({
   className,
   children,
+  
+  typeOfBtn = "white",
   ...props
 }: ButtonProps) => {
   return(
     <button
+   
     className={cn(
-      "text-24 font-semibold border py-2 px-3 rounded-[4px] w-32 h-10   relative flex items-center cursor-pointer ",
+      "text-[14px] font-medium  py-2 px-4 rounded-xs flex items-center cursor-pointer active:scale-[101%] ",
       className,
+      typeOfBtn == "black" ? "bg-black text-white  dark:border hover:bg-black/90  dark:hover:bg-[#08090a] " : " hover:bg-black/2 dark:hover:bg-white/90 dark:bg-white dark:text-black border"
     )}
     {...props}
   >
